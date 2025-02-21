16f336f063e7e998da6be330663569a1bc1bd726
Osama alkhazali <o.a.alkhazali@gmail.com>
2025-02-21 15:45:58 +0300

    cookies

--- Backend/php/php cookies_sessions_get_post/Cookies/index.php
+++ Backend/php/php cookies_sessions_get_post/Cookies/index.php
@@ -8,9 +8,7 @@
 <body>
   <?php for ($i = 1; $i <= 10; $i++): ?>
     <a href="./pages/cookies<?php echo $i; ?>.php">Cookies Task <?php echo $i; ?></a><br>
-  <?php endfor; ?>
-  <?php for ($i = 1; $i <= 10; $i++): ?>
-    <a href="./pages/sessions<?php echo $i; ?>.php">sessions Task <?php echo $i; ?></a><br>
-  <?php endfor; ?>
+  <?php endfor; 
+  ?>
 </body>
 </html>
